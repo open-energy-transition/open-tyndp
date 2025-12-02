b67c2fcd4d678e8b618de420e906086cf9f01e7e
Thomas Gilon <thomas.gilon@openenergytransition.org>
2025-12-03 00:07:03 +0100

    Revert "feat: group e-fuels and exogenous hydrogen demands together"
    This reverts commit 9579624adb753f03d1fdcbfa82ca137bb9142346.

--- scripts/clean_tyndp_benchmark.py
+++ scripts/clean_tyndp_benchmark.py
@@ -252,7 +252,7 @@ def load_benchmark(
         df_converted = pd.concat([df_group, df_other], ignore_index=True)
     # Aggregate hydrogen demand to match input data resolution
     elif table == "hydrogen_demand":
-        group = ["power generation"]
+        group = ["power generation", "e-fuels"]
         df_other = (
             df_converted[
                 (~df_converted.carrier.isin(group)) & (df_converted.carrier != "total")
@@ -260,7 +260,7 @@ def load_benchmark(
             .groupby(["scenario", "year", "unit", "table"])
             .sum()
             .reset_index()
-            .assign(carrier="final energy demand + e-fuels")
+            .assign(carrier="exogenous demand")
         )
         df_group = df_converted[df_converted.carrier.isin(group)]
         df_converted = pd.concat([df_group, df_other], ignore_index=True)
