3af8ed899faf6c7d8da275a99ae49499bba6029c
Markus Groissböck <markus.groissboeck@gmx.at>
2026-02-16 21:19:19 +0100

    bug adjust message (Euro > Meuro)

--- scripts/cba/make_indicators.py
+++ scripts/cba/make_indicators.py
@@ -755,7 +755,7 @@ if __name__ == "__main__":
     indicators["project_id"] = project_id  # assuming format 't123'
     indicators["cba_method"] = method.upper()
     logger.info(
-        f"Project {indicators['project_id']} is {'beneficial' if indicators['is_beneficial'] else 'not beneficial'} for {indicators['cba_method']}. B1 indicator: {indicators['B1_total_system_cost_change']} Euros"
+        f"Project {indicators['project_id']} is {'beneficial' if indicators['is_beneficial'] else 'not beneficial'} for {indicators['cba_method']}. B1 indicator: {indicators['B1_total_system_cost_change']} Meuro"
     )
 
     # Convert to DataFrame and save
