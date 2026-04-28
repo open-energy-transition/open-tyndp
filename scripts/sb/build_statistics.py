c3ba3fa31a6c2b0e4953cd367c7bd7ce5db8ef0d
Thomas Gilon <thomas.gilon@openenergytransition.org>
2026-04-28 16:20:12 +0200

    feat: map biofuels to solid biomass fed

--- scripts/sb/build_statistics.py
+++ scripts/sb/build_statistics.py
@@ -133,25 +133,9 @@ def compute_benchmark(
             .mul(sws, axis=1)
             .sum(axis=1)
             .loc[lambda s: ~s.index.isin(df_countries.index)]
-            .drop(index=["solid biomass"], errors="ignore")
         )
 
-        # Add Biomass to Liquid
-        df_btl = (
-            n.statistics.supply(
-                comps="Link",
-                bus_carrier="oil",
-                carrier=["biomass to liquid", "biomass to liquid CC"],
-                groupby="carrier",
-                aggregate_across_components=True,
-                groupby_time=False,
-            )
-            .mul(sws, axis=1)
-            .sum(axis=1)
-            .rename_axis("bus_carrier")
-        )
-
-        df = pd.concat([df_countries, df_eu, df_btl])
+        df = pd.concat([df_countries, df_eu])
     elif table == "elec_demand":
         grouper = ["carrier"]
         df = (
